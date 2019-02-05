8799a90771c7f0bde869eae93c5bf9a12362a923
ifoxer <romanliseykin@gmail.com>
2019-02-05 12:14:43 +0300

    Задача 16

--- testFileR.java
+++ testFileR.java
@@ -2,7 +2,7 @@ import java.io.*;
 
 //Задача №15
 public class testFileR {
-
+/*
     public static void main(String[] args) {
 
         try(FileReader reader = new FileReader("primer.txt"))
@@ -19,5 +19,33 @@ public class testFileR {
             System.out.println(ex.getMessage());
         }
     }
-}
+ */
 
+//Задача №16
+
+    public static void main(String[] args) {
+
+        String str;
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        System.out.println("Ввести 'stop' для окончания записи ");
+
+        try (FileWriter fw = new FileWriter("primer.txt")) {
+
+            do {
+                System.out.println(": ");
+                str = br.readLine();
+
+                if (str.compareTo("stop") == 0) break;
+
+                str = str + "\r\n";
+                fw.write(str);
+            } while (str.compareTo("stop") != 0);
+
+        } catch (IOException ex) {
+            System.out.println("ошибка ввода-вывода:" + ex);
+
+        }
+
+
+    }
+}
