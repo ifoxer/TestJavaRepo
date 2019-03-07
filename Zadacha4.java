fdd4312c375ef7bcf208a354777994e52729b967
ifoxer <romanliseykin@gmail.com>
2019-03-07 14:09:21 +0300

    изменена Задача №4

--- Zadacha4.java
+++ Zadacha4.java
@@ -1,25 +1,18 @@
 import java.util.Scanner;
 
 public class Zadacha4 {
+    //Задача №4
+    public static void main(String args[]) {
 
-        public static void main(String args[]) {
+        Scanner sc = new Scanner (System.in);
+        System.out.println ("Ввести значение в бинарном формате:");
+        String s = sc.nextLine();
+        int a = Integer.parseInt(s,2);
+        System.out.println (a);
+        sc.close ();
 
-            Scanner sc = new Scanner(System.in);
-            System.out.println("Ввод трех чисел с клавиатуры X,Y,Z :");
-
-            int X = sc.nextInt();
-            int Y = sc.nextInt();
-            int Z = sc.nextInt();
+    }
 
 
-            int A = X+Y+Z;
-            int S = A/3;
-            System.out.println("среднее арифметическое чисел X,Y,Z: "+S);
-            int D = S/2;
-            System.out.println("Деление среднего арифметического на 2 без остатка: "+D);
-            //Если полученное число больше 3, то вывести на экран сообщение "Программа выполнена корректно"
-            if (D > 3);
-            System.out.println("Программа выполнена корректно");
 
-        }
 }
