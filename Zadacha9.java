38db078ae08225e47c876bc50fcfd185563f3358
ifoxer <romanliseykin@gmail.com>
2019-03-10 22:53:49 +0300

    изменение задачи 9

--- Zadacha9.java
+++ Zadacha9.java
@@ -1,2 +1,21 @@
+import java.util.Scanner;
+
 public class Zadacha9 {
+    public static void main(String args[]) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Введите значения массива: ");
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        int c = sc.nextInt();
+        int d = sc.nextInt();
+        int f = sc.nextInt();
+
+        int[] sm = {a,b,c,d,f};
+
+       System.out.println("Вывод элементов массива mass: ");
+        for (int i = 0; i < 5; i++ ){
+            System.out.println(sm[i] * 2);
+        }
+
+    }
 }
