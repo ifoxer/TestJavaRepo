0c4f2b9d7c40a17e0fc9ea26ef6122b1aed281bf
ifoxer <romanliseykin@gmail.com>
2019-03-11 00:57:10 +0300

    ввывод данных из файла

--- Zadacha16.java
+++ Zadacha16.java
@@ -1,2 +1,16 @@
+import java.io.*;
+
 public class Zadacha16 {
+    public static void main(String[] args) {
+        try(FileReader reader = new FileReader("primer.txt"))
+        {
+            int r;
+            while((r=reader.read())!=-1){
+                System.out.print((char)r);
+            }
+        }
+        catch(IOException ex){
+            System.out.println(ex.getMessage());
+        }
+    }
 }
