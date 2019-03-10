4ce616fdd3171820417d3221414b20f3c18b92b1
ifoxer <romanliseykin@gmail.com>
2019-03-11 00:13:54 +0300

    задача 12

--- Zadacha12.java
+++ Zadacha12.java
@@ -1,2 +1,15 @@
+import java.util.Scanner;
+
 public class Zadacha12 {
+    //Задача №12
+    public static void main(String args[]) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Ввод текста: ");
+        String text = sc.nextLine();
+        System.out.println("Вывод текста без пробелов: ");
+
+
+    String prb = text.replaceAll(" ", "");
+        System.out.print(prb);
+    }
 }
