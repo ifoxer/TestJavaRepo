6e30239f0157dd3585881d18a58605fbdb8fece6
ifoxer <romanliseykin@gmail.com>
2019-03-10 22:13:58 +0300

    изменения в задаче 8

--- Zadacha8.java
+++ Zadacha8.java
@@ -1,2 +1,11 @@
 public class Zadacha8 {
+    //Задача №8
+    public static void main(String args[]) {
+        int i = 1;
+        while (i <= 50){
+            System.out.println(i);
+            i++;
+        }
+    }
+
 }
