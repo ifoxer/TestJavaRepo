d21d3e730289c8b9ec80aac6604febc5e2f1c59f
ifoxer <romanliseykin@gmail.com>
2019-03-11 00:13:34 +0300

    задача 11

--- Zadacha11.java
+++ Zadacha11.java
@@ -1,2 +1,15 @@
+import java.util.Scanner;
+
 public class Zadacha11 {
+    //Задача №11
+    public static void main(String args[]) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Ввод текста: ");
+        String text = sc.nextLine();
+        System.out.println("Вывод текста: ");
+        System.out.print(text);
+        }
+
+
+
 }
