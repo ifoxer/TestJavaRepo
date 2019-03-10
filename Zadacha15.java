b3521f94ff6b22e1a1cd01cbca5c46cb1c0b1e99
ifoxer <romanliseykin@gmail.com>
2019-03-11 00:49:20 +0300

    задача 15

--- Zadacha15.java
+++ Zadacha15.java
@@ -1,2 +1,33 @@
+import java.util.Scanner;
+
 public class Zadacha15 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Введите значения массива: ");
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        int c = sc.nextInt();
+        int d = sc.nextInt();
+        int f = sc.nextInt();
+
+        int arr[] = {a, b, c, d, f};
+        bubbleSort(arr);
+    }
+
+    public static void bubbleSort(int[] arr) {
+        for (int i = arr.length - 1; i > 0; i--) {
+            for (int j = 0; j < i; j++) {
+                if (arr[j] > arr[j + 1]) {
+                    int a = arr[j];
+                    arr[j] = arr[j + 1];
+                    arr[j + 1] = a;
+                }
+            }
+        }
+        for (int i = 0; i < arr.length; i++)
+
+
+            System.out.print(arr[i]+", ");
+    }
 }
+
