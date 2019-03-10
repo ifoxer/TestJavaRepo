7f51312f4da6ae535fac6eb5a62d13c5105f1434
ifoxer <romanliseykin@gmail.com>
2019-03-11 00:30:45 +0300

    Задача 14

--- Zadacha14.java
+++ Zadacha14.java
@@ -1,2 +1,14 @@
+import java.util.Scanner;
+
 public class Zadacha14 {
+    public static void main(String args[]) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Ввод числа:");
+        String S = sc.nextLine();
+        System.out.println("String - "+S);
+        int X = Integer.parseInt(S);
+        System.out.println("INT - "+X);
+        double Y = X;
+        System.out.println("double - "+Y);
+    }
 }
