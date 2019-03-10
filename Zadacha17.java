695038dd22566e8e333617e080dff3ba55a37378
ifoxer <romanliseykin@gmail.com>
2019-03-11 01:06:29 +0300

    задача 17

--- Zadacha17.java
+++ Zadacha17.java
@@ -1,2 +1,20 @@
+import java.io.*;
+
 public class Zadacha17 {
+    public static void main(String[] args) {
+        String str;
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        System.out.println("Ввести 'stop' для окончания записи ");
+        try (FileWriter fw = new FileWriter("primer.txt")) {
+            do {
+                System.out.println(": ");
+                str = br.readLine();
+                if (str.compareTo("stop") == 0) break;
+                str = str + "\r\n";
+                fw.write(str);
+            } while (str.compareTo("stop") != 0);
+        } catch (IOException ex) {
+            System.out.println("ошибка ввода-вывода:" + ex);
+        }
+    }
 }
