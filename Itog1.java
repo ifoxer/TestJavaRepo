afaf94df9ea149249fef705855f97c4db380b39a
ifoxer <romanliseykin@gmail.com>
2019-03-13 13:48:45 +0300

    Обновлено

--- Itog1.java
+++ Itog1.java
@@ -1,27 +1,30 @@
 import java.util.Scanner;
 
+
 public class Itog1 {
 
-    public static void main(String[] args) {
 
-        Scanner sc = new Scanner(System.in);
-        System.out.println("Введите значение: ");
-        int j = sc.nextInt();
-        System.out.println("Таблица умножения для числа :"+j);
-
-        for(int i=1;i<11;i++){
-
-            System.out.print(j+"*"+i+"="+(i*j)+"\t\t");
-
+    public static int bin (String base2){
+        char[] chars = base2.toCharArray();
+        int result = 0;
+        int mult = 1;
+        for (int i = base2.length() - 1; i >= 0; i--) {
+            if (chars[i] == '1') {
+                result += mult;
+            }
+            mult *= 2;
         }
+        return result;
+    }
 
-
+    public static void main(String args[]) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Введите значение в бинарном формате: ");
+        String str = sc.nextLine();
+        int a = bin(str);
+        System.out.println(a);
     }
 
 
-
-
 }
-
-
 
